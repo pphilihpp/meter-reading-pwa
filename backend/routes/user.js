34c066bb05129faf0a5cfed49d0dfec173d1c4d5
Wetschi <kujatw@gmail.com>
2021-01-01 16:54:20 +0100

    added request with axios instead of node-fetch, added info to setup the backend server -> readme

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,61 +1,66 @@
 const express       = require('express');
 const router        = express();
 const bodyParser    = require('body-parser');
-const fetch         = require('node-fetch');
+const axios         = require('axios');
 
 //Dotenv -> .env Files
 require('dotenv').config({ path: './vars/.env' });
+
+
+//Variables
+//User - Authenitcation
+const user = process.env.API_USERNAME;
+const pass = process.env.API_PASSWORD;
+const auth = Buffer.from(`${user}:${pass}`, 'utf8').toString('base64');
+let cookieData = '';
 
 
 //Body Parser
 router.use(bodyParser.urlencoded({ extended: false }))
 router.use(bodyParser.json())
 
-//Authentication
-router.post('/login', async (req, res) => {
-    const user = process.env.API_USERNAME;
-    const pass = process.env.API_PASSWORD;
-    const userData = {
-            username: req.body.username,
-            password: req.body.password
-    }
-    const url = process.env.API_URL + '/app/session';
-    const auth = Buffer.from(`${user}:${pass}`, 'utf8').toString('base64');
 
-    const response = await fetch(url, {
-        method: 'POST',
-        //credentials: 'include',
-        headers: { 'Authorization': `Basic ${auth}`, 'Content-Type': 'application/json;charset=UTF-8'},
-        body: JSON.stringify(userData)
-      })
-      .then(resp => resp.json())
-      .catch(err => res.json({message: err}));
-      //.catch(err => console.log(err));
-    res.json(response);
-    //console.log(response);
+
+//Login Route
+router.post('/login', async (req, res, next) => {
+  const userData = {
+      username: req.body.username,
+      password: req.body.password
+  }
+
+  const response = await axios({
+    method: 'POST',
+    withCredentials: true,
+    url: process.env.API_URL + '/app/session',
+    headers: {  'Authorization': `Basic ${auth}`, 'Content-Type': 'application/json' },
+    data: userData
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+  try {
+    console.log(response.headers);
+    console.log(response.headers['x-token']);
+    cookieData = response.headers['set-cookie'];
+  } catch(err) {
+    console.log('Error: ' + err);
+  }
+
 });
 
-//Testing - GET-Data from API
+
+//GET Request - Contract Overview
 router.get('/contracts', async (req, res) => {
-    const url = process.env.API_URL + '/app/meter-reading';
+  const response = await axios({
+    method: 'GET',
+    withCredentials: true,
+    url: process.env.API_URL + '/app/meter-reading',
+    headers: { 'Cookie': cookieData, 'Authorization': `Basic ${auth}` }
+  })
+  //.then(resp => console.log(resp.data));
 
-    const auth = Buffer.from(`${user}:${pass}`, 'utf8').toString('base64');
-
-    const user = process.env.API_USERNAME;
-    const pass = process.env.API_PASSWORD;
-
-    const response = await fetch(url, {
-        method: 'GET',
-        //credentials: 'include',
-        headers: { 'Authorization': `Basic ${auth}`, 'Content-Type': 'application/json;charset=UTF-8' }
-      })
-      .then(res => res.json())
-      .catch(err => res.json({ message: err }));
-
-      //res.json(response[0]);
-      console.log(response[0]);
-
+  res.send(response.data);
 });
-
 
 module.exports = router;
